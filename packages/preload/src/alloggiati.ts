eb6402526556c9ceac6fcbf6a536265f4c57bc8b
OneStepAt4time <manudis2395@gmail.com>
2022-07-24 22:23:29 +0200

    db model file

---
+++ packages/preload/src/alloggiati.ts
@@ -0,0 +1,91 @@
+import type {Nullable} from 'vitest';
+
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const file = path.resolve(__dirname + './../src/alloggiati.db');
+console.log(file);
+const db = new sqlite3.Database('alloggiati.db');
+
+export async function getTable(tableName: string) {
+  const sql = `SELECT * FROM ${tableName}`;
+  return new Promise((resolve, reject) => {
+    db.all(sql, (err: string, rows: Nullable<any>) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rows);
+    });
+  });
+}
+export async function getTipoAlloggiato(codice: Nullable<string> = null, tableName = 'tipo_alloggiato') {
+    let sql = `SELECT * FROM ${tableName}`;
+    if (codice) {
+        sql += ` WHERE codice = '${codice}'`;
+    }
+    // console.log(sql);
+    return new Promise((resolve, reject) => {
+        db.all(sql, (err: string, rows: Nullable<any>) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(rows);
+        });
+    });
+}
+
+export async function getStato(codice: Nullable<string> = null, tableName = 'stati') {
+  let sql = `SELECT * FROM ${tableName}`;
+  if (codice) {
+    sql += ` WHERE codice = '${codice}'`;
+  }
+  // console.log(sql);
+  return new Promise((resolve, reject) => {
+    db.all(sql, (err: string, rows: Nullable<any>) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rows);
+    });
+  });
+}
+
+export async function getComune(codice: Nullable<string> = null, tableName = 'comuni') {
+  let sql = `SELECT * FROM ${tableName}`;
+  if (codice) {
+    sql += ` WHERE codice = '${codice}'`;
+  }
+  // console.log(sql);
+  return new Promise((resolve, reject) => {
+    db.all(sql, (err: string, rows: Nullable<any>) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rows);
+    });
+  });
+}
+
+export async function getDocumento(codice: Nullable<string> = null, tableName = 'documenti') {
+  let sql = `SELECT * FROM ${tableName}`;
+  if (codice) {
+    sql += ` WHERE codice = '${codice}'`;
+  }
+  // console.log(sql);
+  return new Promise((resolve, reject) => {
+    db.all(sql, (err: string, rows: Nullable<any>) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(rows);
+    });
+  });
+}
+
+
+
+// insert into main.tipo_alloggiato (codice, descrizione) values (16, 'OSPITE SINGOLO');
+// insert into main.stati (codice, descrizione, provincia, dataFineVal) values (100000100, 'ITALIA', 'ES', null);
+// insert into main.comuni (codice, descrizione, provincia, dataFineVal) values (401001001, 'AGLIE''', 'TO', null);
+// insert into main.documenti (codice, descrizione) values ('IDENT', 'CARTA DI IDENTITA''');
+// insert into main.province (provincia, descrizione) values ('AG', 'Agrigento');
